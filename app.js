515cc3c53c37827af9aa035dad8549b96acce41a
suletha <suletha.gopi@gmail.com>
2023-11-14 22:45:29 +0530

    c6

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const shopRoute = require('./router/shop')
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoute);
+app.use('/admin',adminRoute);
 app.use(shopRoute);
 app.use((req, res, next) => {
     res.status(404).send('<h1>Page not found</h1>');
--- router/admin.js
+++ router/admin.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/add-product',(req,res,next) => {
-    console.log('In another middleware');
+    //console.log('In another middleware');
     //next();//Allows the next middleware to continue
-    res.setHeader().send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
 });
 
-router.post('/product',(req,res,next) => {
+router.post('/add-product',(req,res,next) => {
     console.log(req.body);
     res.redirect('/');
 });
