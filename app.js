53e80144fb944fe3b1f3aa62485c3cdfbe655318
suletha <suletha.gopi@gmail.com>
2023-11-14 20:40:34 +0530

    c2

--- app.js
+++ app.js
@@ -3,18 +3,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/',(req,res,next) => {
-    console.log('This always Run!!!!');
-    next();//Allows the next middleware to continue
-});
+// app.use('/',(req,res,next) => {
+//     console.log('This always Run!!!!');
+//     next();//Allows the next middleware to continue
+// });
 
-app.use('/',(req,res,next) => {
-    console.log('In another middleware');
-    //next();//Allows the next middleware to continue
-    res.send('<h1>Hello from Express js</h1>')
-});
+
 
 app.use('/add-product',(req,res,next) => {
     console.log('In another middleware');
@@ -22,9 +18,15 @@ app.use('/add-product',(req,res,next) => {
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
 });
 
-app.use('/',(req,res,next) => {
+app.use('/product',(req,res,next) => {
     console.log(req.body);
     res.redirect('/');
+});
+
+app.use('/',(req,res,next) => {
+    //console.log('In another middleware');
+    //next();//Allows the next middleware to continue
+    res.send('<h1>Hello from Express js</h1>')
 });
 
 
