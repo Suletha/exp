17ffd7ad4679955714bb22a464b3a7440dfea99d
suletha <suletha.gopi@gmail.com>
2023-11-17 12:33:22 +0530

    c7

--- router/shop.js
+++ router/shop.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const path = require('path');
 
 router.get('/',(req,res,next) => {
     //console.log('In another middleware');
     //next();//Allows the next middleware to continue
-    res.send('<h1>Hello from Express js</h1>')
+    //res.send('<h1>Hello from Express js</h1>')
+    res.sendFile(path.join(__dirname,'../', 'views', 'shop.html'));
 });
 module.exports = router;
