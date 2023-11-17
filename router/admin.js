e0310f6709a4dcc5fa9f12efa64c356e3707961a
suletha <suletha.gopi@gmail.com>
2023-11-17 14:45:26 +0530

    c9

--- router/admin.js
+++ router/admin.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const path = require('path')
-
+const rootDir = require('../util/path')
 router.get('/add-product',(req,res,next) => {
     //console.log('In another middleware');
     //next();//Allows the next middleware to continue
     //res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
-    res.sendFile(path.join(__dirname,'../', 'views', 'add-product.html'))
+    //res.sendFile(path.join(__dirname,'../', 'views', 'add-product.html'))
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 });
 
 router.post('/add-product',(req,res,next) => {
--- router/shop.js
+++ router/shop.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const path = require('path');
+const rootDir = require('../util/path')
 
 router.get('/',(req,res,next) => {
     //console.log('In another middleware');
     //next();//Allows the next middleware to continue
     //res.send('<h1>Hello from Express js</h1>')
-    res.sendFile(path.join(__dirname,'../', 'views', 'shop.html'));
+    //res.sendFile(path.join(__dirname,'../', 'views', 'shop.html'));
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 module.exports = router;
